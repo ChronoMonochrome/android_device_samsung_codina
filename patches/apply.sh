7d9204d3cbbda19da93ff45abc6a572d36050741
Shilin Victor <chrono.monochrome@gmail.com>
2015-08-06 03:41:01 +0300

    Chromium: fix video playback
    Conflicts:
    	patches/apply.sh
    
    Change-Id: I7c5c90a79bdc1d8282321f23326385ae6954cc4a

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,6 +1,6 @@
 LOCAL_PATH=../../../..
 
-PATCHES="art build external/fuse frameworks/base frameworks/native frameworks/webview \
+PATCHES="art build external/fuse frameworks/base frameworks/native external/chromium_org \
                     libcore packages/services/Telephony/  system/vold system/core"
 
 export CL_RED="\033[31m"
