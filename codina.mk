8e3f5ec1d898adf065e036c056218e8ed08747d0
Shilin Victor <chrono.monochrome@gmail.com>
2016-01-31 13:51:54 +0700

    Don't use prebuilt audioflinger
    Change-Id: I3e4ec538ddbe438851585e7c6d85c1eaa1a0a54c

--- codina.mk
+++ codina.mk
@@ -125,8 +125,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/charger/images/battery_fail.png:root/res/images/charger/battery_fail.png \
 
 # libaudioflinger prebuilt
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/libaudioflinger.so:system/lib/libaudioflinger.so \
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/prebuilt/libaudioflinger.so:system/lib/libaudioflinger.so \
 #    $(LOCAL_PATH)/prebuilt/netd:system/bin/netd
 
 # Misc Packages
@@ -176,7 +176,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
